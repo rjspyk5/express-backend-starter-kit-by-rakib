694b5c11039ad8ca0d3307c188ac2c0b60ed0c21
rjspyk5 <rjspyk5@gmail.com>
2025-01-13 04:11:35 +0600

    make user model

---
+++ model/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "Name is required"] },
+  role: { type: String },
+  email: { type: String, required: [true, "Email is required"], unique: true },
+  number: { type: Number },
+  photo: { type: String },
+  password: { type: String, required: [true, "Password is required"] },
+});
+
+// Hasging password before new user
+userSchema.pre("save", async (next) => {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// create a methods into user model which i can use when i get user data..in user data will have this method also
+userSchema.methods.isPasswordMatched = async (givenPass) => {
+  return await bcrypt.compare(givenPass, this.password);
+};
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
